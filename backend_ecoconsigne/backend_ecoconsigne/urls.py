67e7c8f248b1b397bcfce2b5e3c4f173a8c4fef2
Dan Faehau <danfaehau@gmail.com>
2025-07-03 01:06:47 +1100

    Ajout du préfixe /api/ dans le routeur principal

--- backend_ecoconsigne/backend_ecoconsigne/urls.py
+++ backend_ecoconsigne/backend_ecoconsigne/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include  # 👈 ajoute "include"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('depot.urls')),  # 👈 ajoute cette ligne pour brancher les URL de l’app "depot"
+    path('api/', include('depot.urls')),
+
 ]
