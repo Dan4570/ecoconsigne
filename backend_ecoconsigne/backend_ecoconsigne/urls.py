001918417f7b114629950fbed7e0406b7f0c8da8
Dan Faehau <danfaehau@gmail.com>
2025-07-09 00:58:50 +1100

    Suppression des communes, correction des URLs et ajout page d'accueil

--- backend_ecoconsigne/backend_ecoconsigne/urls.py
+++ backend_ecoconsigne/backend_ecoconsigne/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include  # 👈 ajoute "include"
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Bienvenue sur la page d'accueil !")
 
 urlpatterns = [
+    path('', home, name='home'),  # route pour /
     path('admin/', admin.site.urls),
     path('api/', include('depot.urls')),
-
 ]
--- backend_ecoconsigne/depot/admin.py
+++ backend_ecoconsigne/depot/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import  Ecocitoyen, Depot
 
-# Register your models here.
+admin.site.register(Ecocitoyen)
+admin.site.register(Depot)
